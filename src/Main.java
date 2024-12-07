761d7b954271a8e657f4b43884c24e954e6dc588
LevonNahapetyan <levonnahapetyan@gmail.com>
2024-12-07 07:21:59 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for(int i = 1; i <= 20; i++) {
             System.out.println(i);
+            System.out.println("Hello");
         }
     }
 }
